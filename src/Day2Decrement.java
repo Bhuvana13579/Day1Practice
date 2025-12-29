61833c3e2b0ca27dccce1f72720d4c0896f153ac
Bhuvana416 <bhuvanasreen.edu2023@gmail.com>
2025-12-29 15:46:26 -0600

    Day2

---
+++ src/Day2Decrement.java
@@ -0,0 +1,7 @@
+public class Day2Decrement {
+    public static void main(String[] args)
+    {
+        int b=10;
+        System.out.println(--b);
+    }
+}
--- src/Day2Variableadd.java
+++ src/Day2Variableadd.java
@@ -3,6 +3,6 @@ public static void main(String[] args)
 {
     int a=10;
     int b=20;
-    System.out.println("Addition of 2 numbers is:"+(a+b));
+    System.out.println(a+b);
 }
 }
